262600fd1b5f6b6af0893e08c8cc45ea88c8084f
H1vee <ailedinskyi@gmail.com>
2025-05-05 14:03:49 +0300

    Implemented Interface for JWT Repository

---
+++ internal/domain/repository/IJWTRepository.go
@@ -0,0 +1,10 @@
+package repository
+
+import "github.com/golang-jwt/jwt/v5"
+
+type IJWTRepository interface {
+	GenerateAccessToken(userID uint) (string, error)
+	GenerateRefreshToken(userID uint) (string, error)
+	ValidateToken(tokenStr string) (*jwt.Token, error)
+	ParseClaims(tokenStr string) (*jwt.MapClaims, error)
+}
