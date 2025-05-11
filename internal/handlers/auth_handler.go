e9bcf3eb8019c2e4740542b9d54cb691baf6f086
H1vee <ailedinskyi@gmail.com>
2025-05-11 22:19:55 +0300

    Implemented password validator

--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -4,25 +4,30 @@ import (
 	"MyNote/internal/usecases"
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 )
 
 type AuthHandler struct {
 	authService *usecases.AuthService
+	validator   *validator.Validate
 }
 
-func NewAuthHandler(service *usecases.AuthService) *AuthHandler {
-	return &AuthHandler{authService: service}
+func NewAuthHandler(service *usecases.AuthService, v *validator.Validate) *AuthHandler {
+	return &AuthHandler{
+		authService: service,
+		validator:   v,
+	}
 }
 
 type RegisterRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
 }
 
 type LoginRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,password"`
 }
 
 func (h *AuthHandler) Register(c echo.Context) error {
@@ -30,6 +35,11 @@ func (h *AuthHandler) Register(c echo.Context) error {
 	if err := c.Bind(&req); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid input")
 	}
+
+	if err := h.validator.Struct(req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Validation failed: %v", err.Error())
+	}
+
 	err := h.authService.Register(req.Email, req.Password)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
@@ -41,6 +51,11 @@ func (h *AuthHandler) Login(c echo.Context) error {
 	if err := c.Bind(&req); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "Invalid input")
 	}
+
+	if err := h.validator.Struct(req); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Validation failed: %v", err.Error())
+	}
+
 	tokens, err := h.authService.Login(req.Email, req.Password)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusUnauthorized, err.Error())
