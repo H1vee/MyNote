cdbb1c60df759ddaa68864a114635de8275147ed
H1vee <ailedinskyi@gmail.com>
2025-05-11 22:22:59 +0300

    Connected validator for password.Implemented container. Created routes for Registration/Login

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"MyNote/internal/app"
 	"MyNote/internal/infrastructure/config"
-	"MyNote/internal/infrastructure/db"
+	"MyNote/pkg/validation"
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	v := validator.New()
+	v.RegisterValidation("password", validation.PasswordValidator)
 
 	cfg := config.Load("/media/Akunamatata/Pet-project/MyNote/config/config.yaml")
+	container, err := app.BuildContainer(cfg)
+	if err != nil {
+		fmt.Printf("Failed to build container: %v", err)
+	}
+	e := echo.New()
+	e.POST("/register", container.AuthHandler.Register)
+	e.POST("/login", container.AuthHandler.Login)
 
-	db.ConnectDB(cfg)
+	e.Logger.Fatal(e.Start("127.0.0.1:1323"))
 }
